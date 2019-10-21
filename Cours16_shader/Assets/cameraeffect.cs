a20a2ba68dc2c719945fcf9045df3dbfd75227b5
B533P-14\533 <olivier.allaire00@outlook.fr>
2019-10-21 17:07:50 -0400

    Finiii

---
+++ Cours16_shader/Assets/cameraeffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class cameraeffect : MonoBehaviour {
+    private Material mat;
+	// Use this for initialization
+	void Awake () {
+        mat = new Material(Shader.Find("Hidden/NewImageEffectShader"));
+	}
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        Graphics.Blit(source, destination,mat);
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
